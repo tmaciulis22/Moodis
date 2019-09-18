17e65888db1c6391363e6f55701b68cd2d833f79
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2019-09-18 20:39:51 +0300

    Make Face class a singleton, add const values

--- Moodis/Moodis/Network/Face/Face.cs
+++ Moodis/Moodis/Network/Face/Face.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Moodie.Network.Face
 {
-    class Face
+    public sealed class Face
     {
+        private Face(){}
+
+        private const string SubscriptionKey = "e285e7e510e0493bbaa7ffe5ffc2db77";
+        private const string UriBase =
+            "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        private static Face instance = null;
+        public static Face Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Face();
+                }
+                return instance;
+            }
+        }
     }
-}
+}  

1144e10a01b7fe611fe7df099c1c12ff92c9f9cd
Andrius <busmabusma@gmail.com>
2019-09-29 23:11:04 +0300

    User class

--- Moodis/Moodis/Models/User.cs
+++ Moodis/Moodis/Models/User.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Moodis.Feature.Login
 {
+    [Serializable]
     class User
     {
+        private String username;
+        private String password;
+
+        public User(String username, String password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername()
+        {
+            return this.username;
+        }
+
+        public String getPassword()
+        {
+            return this.password;
+        }
     }
 }

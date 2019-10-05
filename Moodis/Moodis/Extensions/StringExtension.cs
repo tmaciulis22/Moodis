3c373ca00ace44aa02870a3cbaab180052d1eac9
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2019-10-05 15:29:53 +0300

    Rename String.cs to StringExtension.cs

--- Moodis/Moodis/Extensions/StringExtension.cs
+++ Moodis/Moodis/Extensions/StringExtension.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Moodis.Extensions
 {
-    public static class String
+    public static class StringExtension
     {
         public static string FromJsonToString(this string json)
         {

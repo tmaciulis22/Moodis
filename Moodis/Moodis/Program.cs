c7ad286873e8b91de6618b1d912c774b8cfd9703
Aurimas <auruxxas.ar@gmail.com>
2019-10-08 13:09:03 +0300

    Once again fixed checking for internet connection
    Program will not run if there is no internet

--- Moodis/Moodis/Program.cs
+++ Moodis/Moodis/Program.cs
@@ -11,15 +11,45 @@ namespace moodis
 {
     static class Program
     {
+        private const string WarningInRequest = "Connection failed. Is your internet turned on ?";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+
+            if(checkForInternet())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new LoginForm());
+            }
+            else
+            {
+                //Application.Exit();
+                MessageBox.Show(WarningInRequest);
+                return;
+            }
+            
+            
+        }
+
+        private static bool checkForInternet()
+        {
+            try
+            {
+                using (var client = new System.Net.WebClient())
+                {
+                    using (client.OpenRead("https://google.com"))
+                        return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }

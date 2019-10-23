b36ef8d9377ddc3911e4f4a303c9ecab839d99f4
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2019-10-23 20:17:46 +0300

    Finish activity_signin.xml

---
+++ app-android-xamarin/Moodis/Moodis/Feature/SignIn/SignInActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moodis.Feature.SignIn;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Moodis.Feature.SignIn
+{
+    [Activity(Label = "SignInActivity")]
+    public class SignInActivity : Activity
+    {
+        private SignInViewModel signInViewModel = new SignInViewModel();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_signin);
+        }
+    }
+}

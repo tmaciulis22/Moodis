cfc37bbf6456de5cd9b82fd347f4c25638d784ea
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2019-11-02 16:48:53 +0200

    xamarin sux

--- app-android-xamarin/Moodis/Moodis/History/HistoryActivity.cs
+++ app-android-xamarin/Moodis/Moodis/History/HistoryActivity.cs
@@ -11,6 +11,7 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 using Moodis.Extensions;
+using Moodis.Widget;
 
 namespace Moodis.History
 {
@@ -22,6 +23,20 @@ namespace Moodis.History
             base.OnCreate(savedInstanceState);
             this.SetSupportActionBar();
             SetContentView(Resource.Layout.activity_history);
+            AddClickListeners();
+        }
+
+        void AddClickListeners()
+        {
+            var dateInput = FindViewById<EditText>(Resource.Id.datePicker);
+            dateInput.Click += (sender, e) =>
+            {
+                DatePickerFragment frag = DatePickerFragment.NewInstance(delegate (DateTime time)
+                {
+                    dateInput.Text = time.ToLongDateString();
+                });
+                frag.Show(FragmentManager, DatePickerFragment.TAG);
+            };
         }
     }
 }
---
+++ app-android-xamarin/Moodis/Moodis/History/HistoryViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Lifecycle;
+using Moodis.Ui;
+
+namespace Moodis.History
+{
+    class HistoryViewModel : ViewModel
+    {
+        private const string FormatDouble = "N3";
+        private const string ErrorWhenNoEmotionsFound = "data not found";
+        public List<ImageInfo> monthlyList;
+        public List<ImageInfo> dailyList;
+
+        public void UpdateViewModel()
+        {
+            dailyList = new List<ImageInfo>();
+            monthlyList = new List<ImageInfo>();
+        }
+        public string GetAverageEmotionStats(List<ImageInfo> dailyList)
+        {
+            int size;
+            if (dailyList.Count.Equals(0))
+            {
+                return ErrorWhenNoEmotionsFound;
+            }
+            size = dailyList[0].emotions.Length - 1;
+            List<double> confidenceList = new List<double>(new double[size + 1]);
+
+            foreach (ImageInfo imageInfo in dailyList)
+            {
+                var query = imageInfo.emotions.Select(x => x.confidence);
+                var i = 0;
+                foreach (var confidence in query)
+                {
+                    confidenceList[i] = confidenceList[i] + confidence;
+                    i++;
+                }
+            }
+            int index = confidenceList.IndexOf(confidenceList.Max());
+            return dailyList[0].emotions[index].name + " avg: " + (confidenceList[index] / dailyList.Count).ToString(FormatDouble);
+        }
+    }
+}

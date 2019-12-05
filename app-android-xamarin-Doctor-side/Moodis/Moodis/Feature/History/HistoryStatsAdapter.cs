4863aa08081cdac5f16ba8c89f61aff6e0fc2359
Augustas <augissmaso@gmail.com>
2019-12-05 18:02:33 +0200

    merge from dev.

--- app-android-xamarin-Doctor-side/Moodis/Moodis/Feature/History/HistoryStatsAdapter.cs
+++ app-android-xamarin-Doctor-side/Moodis/Moodis/Feature/History/HistoryStatsAdapter.cs
@@ -1,44 +1,73 @@
 ﻿using Android.Support.V7.Widget;
 using Android.Views;
-using Moodis.Feature.SignIn;
+using Microcharts;
+using Moodis.Feature.History;
 using Moodis.Ui;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Moodis.History
 {
-    class HistoryStatsAdapter : RecyclerView.Adapter
+    public class HistoryStatsAdapter : RecyclerView.Adapter
     {
-        private List<ImageInfo> _statList;
+        public const int DAILY_CHART = 0;
+        private const int STAT = 1;
 
-        public HistoryStatsAdapter(IList<ImageInfo> statList)
+        private List<object> _itemList;
+
+        public HistoryStatsAdapter(IList<object> itemList)
         {
-            _statList = statList.ToList();
+            _itemList = itemList.ToList();
         }
 
         public override int ItemCount
         {
-            get { return _statList.Count; }
+            get { return _itemList.Count; }
+        }
+
+        public override int GetItemViewType(int position)
+        {
+            if (_itemList.ElementAt(position) is DonutChart)
+            {
+                return DAILY_CHART;
+            }
+            else
+            {
+                return STAT;
+            }
         }
 
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
-            var viewHolder = holder as StatViewHolder;
-            viewHolder.TimeLabel.Text = _statList[position].ImageDate.Hour.ToString() + ":" + _statList[position].ImageDate.Minute.ToString();
-            viewHolder.EmotionLabel.Text = _statList[position].emotions.Max().Name;
-            viewHolder.UsernameLabel.Text = SignInViewModel.userList.Find(user => user.Id == _statList[position].UserId).Username;
+            if (holder.ItemViewType == STAT)
+            {
+                (holder as StatViewHolder).OnBind(_itemList[position] as ImageInfo);
+            }
+            else if (holder.ItemViewType == DAILY_CHART)
+            {
+                var stats = _itemList.SkipWhile(item => item is Chart).Select(stat => stat as ImageInfo).ToList();
+                (holder as ChartViewHolder).OnBind(stats, _itemList[position] as DonutChart);
+            }
         }
 
         public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
         {
-            View itemView = LayoutInflater.From(parent.Context).
-                Inflate(Resource.Layout.holder_history_stat, parent, false);
-            return new StatViewHolder(itemView);
+            View itemView;
+            if (viewType == STAT)
+            {
+                itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.holder_history_stat, parent, false);
+                return new StatViewHolder(itemView);
+            }
+            else
+            {
+                itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.holder_chart, parent, false);
+                return new ChartViewHolder(itemView);
+            }
         }
 
-        public void UpdateList(IList<ImageInfo> statList)
+        public void UpdateList(IList<object> itemList)
         {
-            _statList = statList.ToList();
+            _itemList = itemList.ToList();
             NotifyDataSetChanged();
         }
     }

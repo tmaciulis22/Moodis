8b8b87a7ad42335b6475f86206af9f3b0bc815e8
Aurimas <auruxxas.ar@gmail.com>
2019-12-09 20:29:10 +0200

    special fix for Augustas <3

--- API/MoodisAPI/apiMoodis/apiMoodis/Models/DataModel.Designer.cs
+++ API/MoodisAPI/apiMoodis/apiMoodis/Models/DataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Workspace\C#\Moodis\API\MoodisAPI\apiMoodis\apiMoodis\Models\DataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Aurimas\Desktop\C#\Moodis\API\MoodisAPI\apiMoodis\apiMoodis\Models\DataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

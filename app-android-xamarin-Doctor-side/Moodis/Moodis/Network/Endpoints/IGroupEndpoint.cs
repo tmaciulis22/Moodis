55bbb204fb1baf2c10e87e59fa0d80ebe1af85a8
Augustas <augissmaso@gmail.com>
2019-12-09 19:38:41 +0200

    temp

---
+++ app-android-xamarin-Doctor-side/Moodis/Moodis/Network/Endpoints/IGroupEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moodis.Feature.Group;
+using Moodis.Network.Requests;
+using Refit;
+
+namespace Moodis.Network.Endpoints
+{
+    interface IGroupEndpoint
+    {
+        [Get("/group?Id={id}")]
+        public Task<Group> GetByIdGroup(string id);
+
+        [Get("/group?doctorId={doctorid}")]
+        public Task<List<Group>> GetDoctorGroups(string doctorid);
+
+        [Get("/group")]
+        public Task<List<Group>> GetAllGroups();
+
+        [Post("/group")]
+        public Task CreateGroup([Body] Group group);
+
+        [Delete("/group?id={id}")]
+        public Task DeleteGroup(string id);
+    }
+}
